6df7a808659ef6e946ee932ba02edb7dcff4da9f
Mark Heiges <mheiges@uga.edu>
2017-09-21 11:07:07 -0400

    WIP: misc. fixes

--- lib/sql/dbsetup/appdbGusPrep.sql
+++ lib/sql/dbsetup/appdbGusPrep.sql
@@ -8,7 +8,7 @@ ALTER PROFILE DEFAULT LIMIT PASSWORD_LIFE_TIME UNLIMITED;
 create bigfile tablespace APIDB datafile '/u02/oradata/&&oracleSid./apidb.dbf' size 100M autoextend on; 
 -- The GUS tablespace is currently included by dbca + ebrc_savm_database.dbt
 -- create bigfile tablespace GUS datafile '/u02/oradata/&&oracleSid./gus.dbf' size 100M autoextend on;
-create bigfile tablespace INDX datafile '/u02/oradata/&&oracleSid./indx.dbf' size 100M autoextend on;
+-- create bigfile tablespace INDX datafile '/u02/oradata/&&oracleSid./indx.dbf' size 100M autoextend on;
 
 alter database default tablespace gus;
 
