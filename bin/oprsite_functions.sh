d389e9075362c2f22e85bfcb336d77116751a2ce
Mark Heiges <mheiges@uga.edu>
2018-03-23 22:27:56 -0400

    allow website HOST value to be overriden when running oprsite

--- bin/oprsite_functions.sh
+++ bin/oprsite_functions.sh
@@ -21,6 +21,11 @@ function startup_checks {
 function write_env_dot_file {
   local source_website="$1"
   local to_file="$2"
+  local dest_website="$3"
   sudo curl -L -f -sL "http://${source_website}/dashboard/xml/vmenv/value" -o "$to_file"
   [[ "$?" -eq "0" ]] || errexit "Unable to create '$to_file'." 
+  if [[ -n $dest_website ]]; then
+    sudo perl -pi -e "s/HOST=.+/HOST=${dest_website}/" "$to_file"
+    [[ "$?" -eq "0" ]] || errexit "Unable to set HOST to '${dest_website}' in '$to_file'."
+  fi
 }
